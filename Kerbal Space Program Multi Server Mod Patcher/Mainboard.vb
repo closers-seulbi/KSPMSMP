a8e0886fabe0b2ef7c26bbfbab5dc6e47ae441aa
closers-seulbi <seulbi.cf@gmail.com>
2016-08-01 12:58:31 +0800

    Form_Tudy Hot Fix
    Assembly Version 1.0.0.2
    Name Version 1.0.0.2
    
    ====================
    
    터디 폼 닫은 후 메인 폼에서 다시 터디 폼을 불러올때 발생하는 문제 해결

--- Kerbal Space Program Multi Server Mod Patcher/Mainboard.vb
+++ Kerbal Space Program Multi Server Mod Patcher/Mainboard.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Mainboard
-    Dim Form_Tudy As New Tudy
     Private Sub Tudy_Click(sender As Object, e As EventArgs) Handles Tudy.Click
+        Dim Form_Tudy As New Tudy
         Form_Tudy.Show()
     End Sub
 End Class
